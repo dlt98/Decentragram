4c9a425d58066e1a7a57ed4e86ec79b53edef8d6
David L. Tomasovic <david.tomasovic@veniture.hr>
2022-04-16 13:52:08 +0200

    Added basic metamask implementation

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,33 @@ import Decentragram from "../abis/Decentragram.json";
 import Navbar from "./Navbar";
 import Main from "./Main";
 
+const loadWeb3 = async () => {
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    await window.ethereum.enable();
+  } else if (window.web3) {
+    window.web3 = new Web3(window.web3.currentProvider);
+  } else {
+    window.alert(
+      "This isn't an ethereum browser. Make sure you have Metamask installed"
+    );
+  }
+};
+
 const App = () => {
   const [account, setAccount] = useState("");
   const [loading, setLoading] = useState(true);
+
+  loadWeb3();
+  loadBlockchainData();
+
+  async function loadBlockchainData() {
+    const web3 = await window.web3;
+    const accounts = await web3.eth.getAccounts();
+
+    setAccount(accounts[0]);
+    setLoading(false);
+  }
 
   return (
     <div>
