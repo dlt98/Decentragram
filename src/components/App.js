73c62a9ed8941d1246eff34f12a05e85b3c753d1
David L. Tomasovic <david.tomasovic@veniture.hr>
2022-04-16 12:43:11 +0200

    Changed from class to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,29 @@
-import React, { Component } from 'react';
-import Web3 from 'web3';
-import Identicon from 'identicon.js';
-import './App.css';
-import Decentragram from '../abis/Decentragram.json'
-import Navbar from './Navbar'
-import Main from './Main'
+import React, { useState } from "react";
+import Web3 from "web3";
+import Identicon from "identicon.js";
+import "./App.css";
+import Decentragram from "../abis/Decentragram.json";
+import Navbar from "./Navbar";
+import Main from "./Main";
 
+const App = () => {
+  const [account, setAccount] = useState("");
+  const [loading, setLoading] = useState(true);
 
-class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        { this.state.loading
-          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : <Main
-            // Code...
-            />
-          }
-        }
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Navbar account={account} />
+      {loading ? (
+        <div id="loader" className="text-center mt-5">
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <Main
+        // Code...
+        />
+      )}
+    </div>
+  );
+};
 
 export default App;
