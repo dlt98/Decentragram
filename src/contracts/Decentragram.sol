57dc6970340e66d1e474c446d0628c2302f4d743
David L. Tomasovic <david.tomasovic@veniture.hr>
2022-04-16 11:45:06 +0200

    Created smart contracts and tests

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,77 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+  string public name = "Decentragram";
+
+
+  //Store images  
+  uint public imageCount = 0;
+  mapping(uint => Image) public images;
+
+  struct Image {
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+
+  event ImageCreated(    
+      uint id,  
+      string hash,
+      string description,
+      uint tipAmount,
+      address payable author
+  );
+
+  event ImageTipped(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount, 
+    address payable author
+  );
+
+  //Create images
+  function uploadImage(string memory _imgHash, string memory _description) public {
+    //Make sure there is an image hash
+    require(bytes(_imgHash).length > 0);
+
+    //Make sure there is an image description
+    require(bytes(_description).length > 0);
+
+    //Make sure its a real address
+    require(msg.sender != address(0x0));
+
+    imageCount++;
+    //Adds an image to the mapping
+    images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+
+    emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+  }
+
+
+  //Tip images
+  function tipImageOwner(uint _id) public payable{
+    //Make sure the id is valid
+    require(_id > 0 && _id <= imageCount);
+
+    //Fetch the image
+    Image memory _image = images[_id];
+
+    //Feth the author
+    address payable _author = _image.author;
+
+    //Pay the author by sending them ETH
+    address(_author).transfer(msg.value);
+
+    //Increment the tip amount
+    _image.tipAmount += msg.value;
+
+    //Update the image
+    images[_id] = _image;
+
+    emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _author);
+  }
+  
 }
